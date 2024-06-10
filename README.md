dff00a4d7f2c05856cd49a5e26cc4ffbd7436e58
the-ivo-robotnic <theivorobotnic@gmail.com>
2024-06-10 17:05:58 -0600

    Init repository

---
+++ README.md
@@ -0,0 +1,20 @@
+# python-project-template
+
+A minimal template python project containing boilerplate that's often re-used.
+
+***
+
+&nbsp;
+
+# Development and Contribution
+
+1. Initialize git-hooks:
+    * `sh -c ./hooks/init-hooks.sh`
+2. Build and install package locally:
+    * `pip install -e .[dev]`
+
+&nbsp;
+
+***
+
+> Created and Maintained by Ivo Robotnic <ivo@elips.is>
---
+++ src/__init__.py
@@ -0,0 +1,20 @@
+APP_VERSION_MAJOR = 0
+APP_VERSION_MINOR = 0
+APP_VERSION_PATCH = 1
+
+# Project Details
+APP_NAME = "lm-calendar"
+APP_VERSION = f"{APP_VERSION_MAJOR}.{APP_VERSION_MINOR}.{APP_VERSION_PATCH}"
+APP_AUTHOR_NAME = "Ivo Robotnic"
+APP_AUTHOR_EMAIL = "ivo@elips.is"
+APP_LICENSE = "MIT"
+APP_DESCRIPTION = "A service for auto-syncing the Speedgaming tournament to a Google calendar."
+APP_URL = "https://github.com/the-ivo-robotnic/lm-calendar"
+APP_URLS = {
+    "bug-tracking": f"{APP_URL}/issues",
+    "documentation": f"{APP_URL}/wiki",
+}
+
+# Maintainer Details
+MAINTAINER_NAME = APP_AUTHOR_NAME
+MAINTAINER_EMAIL = APP_AUTHOR_EMAIL
